206698519e7acb07b33476f643bccfcb6787745a
Makvi-droid <pasbenauro05@gmail.com>
2026-01-13 15:57:00 +0800

    validation testing

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const regform = document.getElementById("regform")
+const first_name = document.getElementById('first-name')
 
 regform.addEventListener("submit", (event) => {
     event.preventDefault()
+
+    if(first_name.value === ''){
+        alert("first name is required")
+    }
 })
