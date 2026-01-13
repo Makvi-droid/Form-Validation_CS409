1909b755f7c2a5f9320c74fb8692e4749ffd8b06
Makvi-droid <pasbenauro05@gmail.com>
2026-01-13 21:54:51 +0800

    making error/validation messages more stylish

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const regform = document.getElementById("regform")
 const first_name = document.getElementById('first-name')
+const last_name = document.getElementById('last-name')
+const username = document.getElementById('username')
 
 regform.addEventListener("submit", (event) => {
     event.preventDefault()
 
     if(first_name.value === ''){
         alert("first name is required")
+    }
+
+    if(last_name.value.trim() === ''){
+        alert("Last name is required")
     }
 })
