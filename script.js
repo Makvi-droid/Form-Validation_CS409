ea5d13073b2a2523a3ce84939e9ec6a9e44fcbca
Makvi-droid <pasbenauro05@gmail.com>
2026-01-13 23:22:08 +0800

    finished form validation designs and testings on one input tag

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-const regform = document.getElementById("regform")
-const first_name = document.getElementById('first-name')
-const last_name = document.getElementById('last-name')
-const username = document.getElementById('username')
+const regform = document.getElementById("regform");
+const first_name = document.getElementById("first-name");
+const last_name = document.getElementById("last-name");
+const username = document.getElementById("username");
 
 regform.addEventListener("submit", (event) => {
-    event.preventDefault()
+  event.preventDefault();
 
-    if(first_name.value === ''){
-        alert("first name is required")
-    }
+  let errorMsg = false;
+  let successMsg = false;
 
-    if(last_name.value.trim() === ''){
-        alert("Last name is required")
-    }
-})
+  if (first_name.value.trim() === "") {
+    showError(first_name, "Firstname is required");
+    errorMsg = true;
+  } else {
+    showSuccess(first_name, "Firstname Approved");
+    successMsg = true;
+  }
+});
+
+const showError = (input, msg) => {
+  const inputParent = input.parentElement;
+  const errorDisplay = inputParent.querySelector(".error-msg");
+  const successDisplay = inputParent.querySelector(".success-msg");
+
+  inputParent.classList.remove("success");
+  successDisplay.innerText = "";
+
+  inputParent.classList.add("error");
+  errorDisplay.innerText = msg;
+};
+
+const showSuccess = (input, msg) => {
+  const succesParent = input.parentElement;
+  const successDispay = succesParent.querySelector(".success-msg");
+  const errorDisplay = succesParent.querySelector(".error-msg");
+
+  succesParent.classList.remove("error");
+  errorDisplay.innerText = "";
+
+  succesParent.classList.add("success");
+  successDispay.innerText = msg;
+};
